9d565eabb45ae5985a3008a0b1666eaf5a3b8037
Zippy-boy <zippy-boy@outlook.com>
2023-12-05 08:53:03 +0000

    f

--- main.ps1
+++
@@ -1,6 +0,0 @@
-
-$url = "https://github.com/Zippy-boy/remoteShutdown/raw/main/web.exe"
-$output = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\web.exe"
-
-Invoke-WebRequest -Uri $url -OutFile $output
-Start-Process powershell.exe -ArgumentList "-File $output"
--- web.go
+++ web.go
@@ -26,14 +26,66 @@ func main() {
 		}
 		fmt.Fprint(w, `
 			<html>
+				<head>
+					<style>
+						body {
+							font-family: Arial, sans-serif;
+							margin: 20px;
+							padding: 0;
+							background-color: #f2f2f2;
+						}
+						.container {
+							max-width: 400px;
+							margin: 0 auto;
+							padding: 20px;
+							background-color: #fff;
+							border-radius: 5px;
+							box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+						}
+						.form-group {
+							margin-bottom: 20px;
+						}
+						.form-group label {
+							display: block;
+							margin-bottom: 5px;
+							font-weight: bold;
+						}
+						.form-group input {
+							width: 100%;
+							padding: 10px;
+							border: 1px solid #ccc;
+							border-radius: 3px;
+						}
+						#butty {
+							padding: 10px 20px;
+							background-color: #4CAF50;
+							color: #fff;
+							border: none;
+							border-radius: 3px;
+							cursor: pointer;
+							font-size: 16px;
+							transition: background-color 0.3s ease;
+							width: 100%;
+						}
+						#butty:hover {
+							background-color: #45a049;
+						}
+					</style>
+				</head>
 				<body>
-					<form method="POST">
-						<label for="pin">Enter PIN:</label>
-						<input type="password" id="pin" name="pin" required>
-						<label for="time">Enter time in seconds:</label>
-						<input type="number" id="time" name="time" required>
-						<button type="submit">Shutdown</button>
-					</form>
+					<div class="container">
+						<form method="POST">
+							<div class="form-group">
+								<label for="pin">Enter PIN:</label>
+								<input type="password" id="pin" name="pin" required>
+							</div>
+							<div class="form-group">
+								<label for="time">Enter time in seconds:</label>
+								<input type="number" id="time" name="time" required>
+							</div>
+							<button id="butty" type="submit">Shutdown</button>
+						</form>
+					</div>
 				</body>
 			</html>
 		`)
